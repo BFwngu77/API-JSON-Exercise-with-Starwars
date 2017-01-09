b8b481e355bccc0fafb497fc1a700321d24517f2
BrettForeman <bfcollege7@yahoo.com>
2017-01-09 11:14:37 -0800

    Initial Commit

---
+++ JSONAPIExercise2Tests/JSONAPIExercise2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  JSONAPIExercise2Tests.swift
+//  JSONAPIExercise2Tests
+//
+//  Created by Brett Foreman on 1/9/17.
+//  Copyright © 2017 Brett Foreman. All rights reserved.
+//
+
+import XCTest
+@testable import JSONAPIExercise2
+
+class JSONAPIExercise2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
