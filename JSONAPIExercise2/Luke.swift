bdec4e5ec8aa761d1de708022e2510987f122d34
BrettForeman <bfcollege7@yahoo.com>
2017-01-10 10:19:43 -0800

    First Commit of my Luke Skywalker exercise with API/JSON

--- JSONAPIExercise2/Luke.swift
+++ JSONAPIExercise2/Luke.swift
@@ -54,15 +54,60 @@ class Luke {
     }
                                             // add download complete to constants folder 
     func downloadLukeDetails(completed: @escaping DownloadComplete) {
+        
         //initialize the URL to tell Alamofire where to download from...
         // git request
         // after we request it we want to give it a response...
+        // Pulling data into our app using Alamofire, and saving it into JSON... Which we will parse later
+        
         Alamofire.request(BASE_URL).responseJSON { response in 
-            let result = response.result // every request has a response, every response has a result. We've now saved the JSON that we want...
-            print(response)
+            let result = response.result
+            
+        // Every request has a response, every response has a result. We've now saved the JSON that we want...
+        // Now we will begin to parse the data by creating a dictionary in which we will collect what we want and then parse it... Also, we've created a dict from above "let result"...
+            
+        if let dict = result.value as? Dictionary<String, Any> { // could be just "any"\
+            
+        // essential to know if one needs to be outside the others brackets... ie, when a dictionary is in anothers dictionary...
+        // try a dict in a dict(hard) lecture 95 (6:30)
+        // When hitting run after this it should show the parsed data... 
+            
+            //1
+                if let name = dict["name"] as? String {
+                    self._characterName = name.capitalized
+                    print(self.characterName)                                           }
+            //2
+                if let height = dict["height"] as? String {
+                self._characterHeight = height.capitalized
+                    print(self.characterHeight)
+                }
+            //3
+                // noticed that mass had to be in both places ...
+                if let mass = dict["mass"] as? String {
+                self._characterWeight = mass.capitalized
+                    print(self.characterWeight)
+                }
+            //4
+                if let birth_year = dict["birth_year"] as? String {
+                self._characterDOB = birth_year.capitalized
+                    print(self.characterDOB)
+                }
+            //5
+                if let gender = dict["gender"] as? String {
+                self._characterGender = gender.capitalized
+                    print(self._characterGender)
+                }
+            //6 // test one with rooted/nested dictionary getting parsed...
+            
+            }
+            completed() // now we need to go to main VC to call the function, so that whenever the view loads we want to load the function and make it work, using a variable. To solve this I needed to make sure it wasn't under the next bracket... it's confusing but don't follow the videos too close when making your own customized one...
+
         }
-        completed() // now we need to go to main VC to call the function, so that whenever the view loads we want to load the function and make it work, using a variable.  
     }
 
-
 }
+
+
+
+
+
--- JSONAPIExercise2/ViewController.swift
+++ JSONAPIExercise2/ViewController.swift
@@ -7,24 +7,38 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var characterNameLbl: UILabel!
+    @IBOutlet weak var characterHeightLbl: UILabel!
+    @IBOutlet weak var characterWeightLbl: UILabel!
+    @IBOutlet weak var characterDOBLbl: UILabel!
+    @IBOutlet weak var characterGenderLbl: UILabel!
     
-    var luke = Luke() // create a generic class for our class Luke, next go to viewDidLoad
+     var luke = Luke() // create a generic class for our class Luke, next go to viewDidLoad
     
-    override func viewDidLoad() {
+        override func viewDidLoad() {
         super.viewDidLoad()
-       
+      
         luke.downloadLukeDetails {
-            // setup UI to load downloaded data
-        }
-    
-        
+            self.updateMainUI()
+// use self bc it's closure...
+// setup UI to load downloaded data
+// create below and add the function in this viewDidLoad after
     }
-
-   
-
-
 }
-
+    
+    
+    func updateMainUI(){
+// as soon as it's done downloading the data, it will update our labels shown below...
+        characterNameLbl.text = luke.characterName
+        characterHeightLbl.text = "\(luke.characterHeight) \("m")"
+        characterWeightLbl.text = "\(luke.characterWeight) \("Kg")"
+        characterDOBLbl.text = luke.characterDOB
+        characterGenderLbl.text = luke.characterGender
+    }
+   
+}
+    
